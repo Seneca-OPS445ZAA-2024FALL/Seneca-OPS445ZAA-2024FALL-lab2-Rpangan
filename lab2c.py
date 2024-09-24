32da94959d858d25e6cd786d7d687b3d6f286895
Roniel Pangan <roniel.pangan06@gmail.com>
2024-09-23 20:59:43 -0400

    Added lab2c.py

---
+++ lab2c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+import sys
+
+name = sys.argv[1]  # First argument
+age = int(sys.argv[2])  # Second argument, converted to an integer
+
+print('Hi ' + name + ', you are ' + str(age) + ' years old.')
+
