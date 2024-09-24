bd2d9382c59e1e3691679fd266d3b8ab6791cd3e
Roniel Pangan <roniel.pangan06@gmail.com>
2024-09-23 21:22:33 -0400

    Added lab2e.py

---
+++ lab2e.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# Author: Roniel G. Pangan
+# Author ID: 113061220
+# Date Created: 2024/09/23
+
+timer = 10
+while timer > 0:
+    print(timer)
+    timer -= 1
+print('blast off!')
