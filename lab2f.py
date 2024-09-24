ba8655ffe43bca3cb819908bcf09907680fb566e
Roniel Pangan <roniel.pangan06@gmail.com>
2024-09-24 00:47:01 -0400

    Added lab2f.py

---
+++ lab2f.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# Author: Roniel G. Pangan
+# Author ID: 113061220
+# Date Created: 2024/09/23
+
+import sys
+
+timer = int(sys.argv[1])
+while timer > 0:
+    print(timer)
+    timer -= 1
+print('blast off!')
